6ff51076d9793dd98cf061d12f6c010a6168474b
seyio91 <seyi_obaweya@yahoo.com>
2020-07-16 09:21:43 +0200

    create terraform providers

--- dokubernetes.tf
+++ dokubernetes.tf
@@ -33,6 +33,23 @@ resource "kubernetes_namespace" "ether-api-prod" {
   }
 }
 
+resource "kubernetes_namespace" "argo-namespace" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "kubernetes_namespace" "cert-manager" {
+  metadata {
+    name = "cert-manager"
+  }
+}
+
+resource "kubernetes_namespace" "ingress-resource" {
+  metadata {
+    name = "ingress-nginx"
+  }
+}
 
 resource "kubernetes_secret" "atlasname" {
    metadata {
@@ -41,7 +58,7 @@ resource "kubernetes_secret" "atlasname" {
    }
    
    type = "Opaque"
-   data {
+   data = {
      "atlas_db_name" = var.atlas_db_name
      "atlas_password" = var.atlas_password
      "jwtsecret" = var.jwtsecret
@@ -67,7 +84,7 @@ resource "helm_release" "cluster-ingress" {
     name      = "ingress-nginx"
     chart     = "stable/nginx-ingress"
     repository = data.helm_repository.stable.metadata.0.name
-    namespace  = "ingress-namespace"
+    namespace  = "ingress-nginx"
 
     set {
         name  = "controller.publishService.enabled"
@@ -87,6 +104,20 @@ resource "helm_release" "argo-release" {
     repository = data.helm_repository.argo.metadata.0.name
 }
 
+
+data "helm_repository" "jetstack" {
+  name = "jetstack"
+  url  = "https://charts.jetstack.io"
+}
+
+resource "helm_release" "jetstack-release" {
+    name      = "cert-manager"
+    chart     = "jetstack/cert-manager"
+    namespace = "cert-manager"
+    version   = "v0.14.1"
+    repository = data.helm_repository.argo.metadata.0.name
+}
+
 provider "kubectl" {
   host = digitalocean_kubernetes_cluster.dokub3.endpoint
 
@@ -96,35 +127,29 @@ provider "kubectl" {
   load_config_file       = false
 }
 
-
-resource "kubectl_manifest" "prod-api" {
-  yaml_body=<<YAML
-    apiVersion: argoproj.io/v1alpha1
-    kind: Application
+resource "kubectl_manifest" "cert-issuer" {
+  yaml_body = <<YAML
+    apiVersion: cert-manager.io/v1alpha2
+    kind: ClusterIssuer
     metadata:
-      finalizers:
-      - resources-finalizer.argocd.argoproj.io
-      name: etherbank-api-prod
-      namespace: argocd
+      name: letsencrypt-prod
+      labels:
+        name: letsencrypt-prod
     spec:
-      destination:
-        namespace: etherbank-api-prod
-        server: https://kubernetes.default.svc
-      project: default
-      source:
-        path: prod
-        repoURL: https://github.com/codetobank-team/etherbank-infra-backend
-        targetRevision: master
-      syncPolicy:
-        automated:
-          prune: true
-          selfHeal: false
+      acme:
+        email: seyi_obaweya@yahoo.com
+        privateKeySecretRef:
+          name: letsencrypt-prod
+        server: https://acme-v02.api.letsencrypt.org/directory
+        solvers:
+        - http01:
+            ingress:
+              class: nginx
   YAML
 }
 
-
 resource "kubectl_manifest" "prod-client" {
-  yaml_body=<<YAML
+  yaml_body = <<YAML
     apiVersion: argoproj.io/v1alpha1
     kind: Application
     metadata:
@@ -140,6 +165,31 @@ resource "kubectl_manifest" "prod-client" {
       source:
         path: prod
         repoURL: https://github.com/codetobank-team/etherbank-infra
+        targetRevision: master
+      syncPolicy:
+        automated:
+          prune: true
+          selfHeal: false
+  YAML
+}
+
+resource "kubectl_manifest" "prod-api" {
+  yaml_body = <<YAML
+    apiVersion: argoproj.io/v1alpha1
+    kind: Application
+    metadata:
+      finalizers:
+      - resources-finalizer.argocd.argoproj.io
+      name: etherbank-api-prod
+      namespace: argocd
+    spec:
+      destination:
+        namespace: etherbank-api-prod
+        server: https://kubernetes.default.svc
+      project: default
+      source:
+        path: prod
+        repoURL: https://github.com/codetobank-team/etherbank-infra-backend
         targetRevision: master
       syncPolicy:
         automated:
